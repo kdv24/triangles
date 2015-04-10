d868d42318ae58dc4802c645bc6f7a8771b62ede
Kelly de Vries <kdv@me.com>
2015-04-10 10:08:49 -0700

    Add passing test for evaluating an equilateral triangle.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,26 @@
 //check to see if lengths can combine to be valid triangle or not
 var triangleChecker = function(length1, length2, length3){
     if (
-        (length1 + length2) > length3 ||
-        (length2 + length3) > length1 ||
-        (length3 + length1) > length2
+        (length1 + length2) < length3 ||
+        (length2 + length3) < length1 ||
+        (length3 + length1) < length2
         ) {
+//if no, then return a message saying isn't a triangle
+//if yes, then check to see if three sides are equal and return "equilateral"
             return "not a triangle";
-        }
+        } else if((length1 === length2) && (length2 === length3)){
+                return "equilateral";
+            }
 
-};
 
-//if no, then return a message indicating that the numbers they entered do not create a triangle
 
-//if yes, then loop through sides to check to see if two sides are equal
+        //if no, then return "isosceles"
+
+
 
     //if no, then return "scalene"
 
     //if yes, then check to see if one of those sides is equal to the third side
 
-        //if no, then return "isosceles"
 
-        //if yes, then return "equilateral"
+    };
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,8 @@
 describe("triangleChecker", function(){
-    it("will return 'not a triangle' if user enters '3, 4, 9'", function(){
+    it("will return 'not a triangle' if user enters digits that can't make a triangle- '3, 4, 9'", function(){
         expect(triangleChecker(3, 4, 9)).to.equal('not a triangle');
+    });
+    it ("will return 'equilateral' if user enters digits that are all equal- '4, 4, 4'", function() {
+        expect(triangleChecker(4, 4, 4)).to.equal('equilateral');
     });
 });
