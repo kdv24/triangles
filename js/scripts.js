a9c44540cc615475ee8f4703060b9ef902d4c425
Kelly de Vries <kdv@me.com>
2015-04-10 09:26:15 -0700

    Add commenting of logic in scripts.js.

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+//check to see if lengths can combine to be valid triangle or not
+
+//if no, then return a message indicating that the numbers they entered do not create a triangle
+
+//if yes, then loop through sides to check to see if two sides are equal
+
+    //if no, then return "scalene"
+
+    //if yes, then check to see if one of those sides is equal to the third side
+
+        //if no, then return "isosceles"
+
+        //if yes, then return "equilateral"
