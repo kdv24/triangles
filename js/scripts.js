bfa030c93ccb43846717e48acb4b6e953108d83d
Kelly de Vries <kdv@me.com>
2015-04-10 10:46:14 -0700

    Add initial form to triangle.html and also initial jQuery input in scripts.js.

--- js/scripts.js
+++ js/scripts.js
@@ -5,19 +5,30 @@
 //if not, then return "isosceles"
 
 var triangleChecker = function(length1, length2, length3){
-    if (
-        (length1 + length2) < length3 ||
-        (length2 + length3) < length1 ||
-        (length3 + length1) < length2
-        ) {
-            return "not a triangle";
-        }
-    else if ((length1 !== length2) && (length2 !== length3) && (length3 !== length1)){
-            return "scalene";
-        }
-    else if((length1 === length2) && (length2 === length3)){
-                return "equilateral";
-        } else {
-                return "isosceles";
-        }
-    };
+  if (
+    (length1 + length2) < length3 ||
+    (length2 + length3) < length1 ||
+    (length3 + length1) < length2
+    ) {
+    return "not a triangle";
+    }
+  else if ((length1 !== length2) && (length2 !== length3) && (length3 !== length1)){
+    return "scalene";
+    }
+  else if((length1 === length2) && (length2 === length3)){
+    return "equilateral";
+    } else {
+    return "isosceles";
+    }
+};
+
+$(document).ready(function() {
+  $("#user-input form").submit(function(event){
+    var length1 = parseInt($("input#length1").val());
+    var length2 = parseInt($("input#length2").val());
+    var length3 = parseInt($("input#length3").val());
+    var result = triangleChecker(length1, length2, length3);
+
+    event.preventDefault();
+  });
+});
