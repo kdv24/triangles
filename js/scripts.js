e7e2cf8e0acbbaefd64757f2bd791a32a0b1d245
Kelly de Vries <kdv@me.com>
2015-04-10 10:29:24 -0700

    Add passing test for evaluating a scalene triangle. Not pretty and concise yet, but functional.

--- js/scripts.js
+++ js/scripts.js
@@ -8,10 +8,19 @@ var triangleChecker = function(length1, length2, length3){
 //if no, then return a message saying isn't a triangle
 //if yes, then check to see if three sides are equal and return "equilateral"
             return "not a triangle";
-        } else if((length1 === length2) && (length2 === length3)){
-                return "equilateral";
-            }
+        }
+        // else if((length1 === length2) && (length2 === length3)){
+        //         return "equilateral";
+        //     }
+        else if ((length1 !== length2) && (length2 !== length3) && (length3 !== length1)){
+            return "scalene";
+        }
 
+    else if((length1 === length2) && (length2 === length3)){
+                return "equilateral";
+            } else {
+                return "isosceles";
+            }
 
 
         //if no, then return "isosceles"
@@ -19,6 +28,7 @@ var triangleChecker = function(length1, length2, length3){
 
 
     //if no, then return "scalene"
+
 
     //if yes, then check to see if one of those sides is equal to the third side
 
--- spec/specs.js
+++ spec/specs.js
@@ -5,4 +5,10 @@ describe("triangleChecker", function(){
     it ("will return 'equilateral' if user enters digits that are all equal- '4, 4, 4'", function() {
         expect(triangleChecker(4, 4, 4)).to.equal('equilateral');
     });
+    it("will return 'isosceles' if user enters 2 digits that are equal and 1 that isn't- '3, 4, 4'", function() {
+        expect(triangleChecker(3, 4, 4)).to.equal('isosceles');
+    });
+    it("will return 'scalene' if user enters 3 different digits that form a triangle- '4, 6, 7'", function() {
+        expect(triangleChecker(4, 6, 7)).to.equal('scalene');
+    });
 });
