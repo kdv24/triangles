bce466f243484766cc01f61ff76fe401659baef5
Kelly de Vries <kdv@me.com>
2015-04-10 10:33:25 -0700

    Clean up spacing and commenting in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,23 @@
 //check to see if lengths can combine to be valid triangle or not
+//if no, then return a message saying isn't a triangle
+//if yes, then check to see if three sides are not equal, then return "scalene"
+//if not, then check to see if three sides are equal and return "equilateral"
+//if not, then return "isosceles"
+
 var triangleChecker = function(length1, length2, length3){
     if (
         (length1 + length2) < length3 ||
         (length2 + length3) < length1 ||
         (length3 + length1) < length2
         ) {
-//if no, then return a message saying isn't a triangle
-//if yes, then check to see if three sides are equal and return "equilateral"
             return "not a triangle";
         }
-        // else if((length1 === length2) && (length2 === length3)){
-        //         return "equilateral";
-        //     }
-        else if ((length1 !== length2) && (length2 !== length3) && (length3 !== length1)){
+    else if ((length1 !== length2) && (length2 !== length3) && (length3 !== length1)){
             return "scalene";
         }
-
     else if((length1 === length2) && (length2 === length3)){
                 return "equilateral";
-            } else {
+        } else {
                 return "isosceles";
-            }
-
-
-        //if no, then return "isosceles"
-
-
-
-    //if no, then return "scalene"
-
-
-    //if yes, then check to see if one of those sides is equal to the third side
-
-
+        }
     };
